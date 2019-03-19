f51fae7b093148cee1465822f5f762c873cb20bc
= <=>
2019-03-19 15:43:27 -0300

    tópicos, respostas, visualizações

--- simplemooc/forum/models.py
+++ simplemooc/forum/models.py
@@ -2,18 +2,26 @@ from django.db import models
 from taggit.managers import TaggableManager
 from django.conf import settings
 
+
 class Thread(models.Model):
     title = models.CharField('Título', max_length=100)
     body = models.TextField('Mensagem')
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='Autor', related_name='threads', on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='Autor',
+                               related_name='threads', on_delete=models.CASCADE)
     views = models.IntegerField('Visualizações', blank=True, default=0)
     answers = models.IntegerField('Respostas', blank=True, default=0)
     tags = TaggableManager()
     created_at = models.DateTimeField('Criado em', auto_now_add=True)
     updated_at = models.DateTimeField('Atualizado em', auto_now=True)
+    slug = models.SlugField('Identificador', max_length=100,
+                            unique=True, blank=True, null=True)
 
     def __str__(self):
         return self.title
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('forum:thread', (), {'slug': self.slug})
 
     class Meta:
         verbose_name = 'Tópico'
@@ -22,9 +30,11 @@ class Thread(models.Model):
 
 
 class Reply(models.Model):
-    thread = models.ForeignKey(Thread, verbose_name='Tópico', related_name='replies', on_delete=models.CASCADE)
+    thread = models.ForeignKey(
+        Thread, verbose_name='Tópico', related_name='replies', on_delete=models.CASCADE)
     message = models.TextField('Mensagem')
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='Autor', related_name='replies', on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='Autor',
+                               related_name='replies', on_delete=models.CASCADE)
     correct = models.BooleanField('Melhor resposta', blank=True, default=False)
     created_at = models.DateTimeField('Criado em', auto_now_add=True)
     updated_at = models.DateTimeField('Atualizado em', auto_now=True)
@@ -37,4 +47,21 @@ class Reply(models.Model):
         verbose_name_plural = 'Respostas'
         ordering = ['-correct', 'created_at']
 
-    
+
+def post_save_reply(created, instance, **kwargs):
+    instance.thread.answers = instance.thread.replies.count()
+    instance.thread.save()
+
+
+def post_delete_reply(isinstance, **kwargs):
+    instance.thread.answers = instance.thread.replies.count()
+    instance.thread.save()
+
+
+models.signals.post_save.connect(
+    post_save_reply, sender=Reply, dispatch_uid='post_save_reply'
+)
+
+models.signals.post_delete.connect(
+    post_delete_reply, sender=Reply, dispatch_uid='post_delete_reply'
+)
--- simplemooc/forum/urls.py
+++ simplemooc/forum/urls.py
@@ -5,4 +5,6 @@ app_name = 'forum'
 
 urlpatterns = [
     path('', v.index, name='index'),    
+    path('tag/<str:tag>/', v.index, name='index_tagged'),
+    path('topico/<slug:slug>/', v.thread, name='thread'),
 ]
