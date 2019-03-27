dde35c03a7fce17feaa424962b3ab47e2cf5efee
= <=>
2019-03-27 15:43:25 -0300

    wsgi

--- simplemooc/wsgi.py
+++ simplemooc/wsgi.py
@@ -1,8 +1,7 @@
 import os
-from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "simplemooc.settings")
 
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
