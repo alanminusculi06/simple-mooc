b783db162429a561c81fe3f983bc32d11eff80d5
= <=>
2019-03-27 11:43:12 -0300

    settings

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -146,9 +146,9 @@ STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 try:
     from simplemooc.local_settings import *
 except ImportError:
